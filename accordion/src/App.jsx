99a98fb8f2083a9b0f2afb82fe7eb15165dbb146
Antonio <antoniojsp@gmail.com>
2025-10-16 01:21:30 -0700

    working in load more data

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -8,11 +8,12 @@ function App() {
 
   return (
     <div className="App">
-      {/* <Accordian />
+      <Accordian />
       <RandomColor />
       <StarRating numStars={10} />
-      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} /> */}
-      <LoadMoreData url="https://dummyjson.com/products"/>
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={1} limit={10} />
+      <LoadMoreData
+        url="https://dummyjson.com/products" />
 
     </div>
   )
--- accordion/src/components/load-more-data/index.jsx
+++ accordion/src/components/load-more-data/index.jsx
@@ -2,24 +2,26 @@ import { useEffect, useState } from "react"
 
 export default function LoadMoreData({url}){
 
-    const [loading, setLoading] = useState();
-    const [products, setProducts] = useState();
-    const [count, setCount] = useState();
+    const [loading, setLoading] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0 );
 
-    async function fetchProducts(getUrl){
+    async function fetchProducts(){
         try{
-            const response = await fetch(getUrl);
+            const response = await fetch();
             const data = await response.json()
             console.log(data);
+             if(data){
+                setProducts(data.results)
+             }
         }catch(e){
             console.log(e)
         }
     }
 
     useEffect(() => {
-        if (url != ""){
-            fetchProducts(url)
-        }}, [])
+            fetchProducts()
+        }, [])
  
 
     return (
