bd82df1bf7c714f0326fc293537f5e4fc4de96ee
Antonio <antoniojsp@gmail.com>
2025-11-06 03:00:26 -0800

    tick tac handle click progressing

--- accordion/src/components/tic-tac-toe/index.jsx
+++ accordion/src/components/tic-tac-toe/index.jsx
@@ -1,30 +1,37 @@
 import "./styles.css"
-
-function Square({val}){
+import {useState} from "react"
+function Square({val, onClick}){
     return(
-        <button className="square">{val}</button>
+        <button onClick={onClick}className="square">{val}</button>
     )
 }
 
 
 export default function TicTacToe(){
 
+    const [squares, setSquares] = useState([...Array(9)].map(() => ''))
+    const [isXTurn, setIsXTurn] = useState(true);
+    function handleClick(index){
+        setSquares(x => );
+    }
+
+
     return (
         <div className="tic-tac-toe-container">
             <div className="row">
-                <Square  val={""}/>
-                <Square  val={""}/>
-                <Square  val={""}/>
+                <Square  onClick={()=>handleClick(0)}/> 
+                <Square  onClick={()=>handleClick(1)}/>
+                <Square  onClick={()=>handleClick(2)}/>
             </div>
             <div className="row">
-                <Square  val={""}/>
-                <Square  val={""}/>
-                <Square  val={""}/>
+                <Square onClick={()=>handleClick(3)}/>
+                <Square onClick={()=>handleClick(4)}/>
+                <Square onClick={()=>handleClick(5)}/>
             </div>            
             <div className="row">
-                <Square  val={""}/>
-                <Square  val={""}/>
-                <Square  val={""}/>
+                <Square  onClick={()=>handleClick(6)}/>
+                <Square  onClick={()=>handleClick(7)}/>
+                <Square  onClick={()=>handleClick(9)}/>
             </div>
 
         </div>
