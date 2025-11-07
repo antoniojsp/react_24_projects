1700fa3e3184702fbbeb3ce3576b55019ebb4bc7
antonio Silva <antonios@uoregon.edu>
2025-11-06 16:11:04 -0800

    for loop, for you

--- accordion/src/components/tic-tac-toe/index.jsx
+++ accordion/src/components/tic-tac-toe/index.jsx
@@ -1,37 +1,80 @@
 import "./styles.css"
-import {useState} from "react"
-function Square({val, onClick}){
-    return(
-        <button onClick={onClick}className="square">{val}</button>
+import { useEffect, useState } from "react"
+
+function Square({ val, onClick, disabled }) {
+    return (
+        <button
+            onClick={onClick}
+            className="square"
+            disabled={disabled}
+        >
+            {val}
+        </button>
     )
 }
 
-
-export default function TicTacToe(){
+export default function TicTacToe() {
 
     const [squares, setSquares] = useState([...Array(9)].map(() => ''))
-    const [isXTurn, setIsXTurn] = useState(true);
-    function handleClick(index){
-        setSquares(x => );
+    const [turn, setTurn] = useState("X")
+
+    function CheckGame() {
+        const winners = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for ()
     }
 
+    function handleClick(index) {
+        setSquares(prev => prev.map((x, i) => i === index ? turn : x));
+        setTurn(x => x === "X" ? "O" : "X")
+    }
+    useEffect(x => {
+        console.log(squares)
+    }, [squares])
 
     return (
         <div className="tic-tac-toe-container">
             <div className="row">
-                <Square  onClick={()=>handleClick(0)}/> 
-                <Square  onClick={()=>handleClick(1)}/>
-                <Square  onClick={()=>handleClick(2)}/>
+                {
+                    squares.map((x, i) => [0, 1, 2].includes(i) ? <Square
+                        key={i}
+                        onClick={() => handleClick(i)}
+                        val={squares[i]}
+                        disabled={squares[i] != ""}
+                    /> : null
+                    )
+                }
             </div>
             <div className="row">
-                <Square onClick={()=>handleClick(3)}/>
-                <Square onClick={()=>handleClick(4)}/>
-                <Square onClick={()=>handleClick(5)}/>
-            </div>            
+                {
+                    squares.map((x, i) => [3, 4, 5].includes(i) ? <Square
+                        key={i}
+                        onClick={() => handleClick(i)}
+                        val={squares[i]}
+                        disabled={squares[i] != ""}
+                    /> : null
+                    )
+                }
+            </div>
             <div className="row">
-                <Square  onClick={()=>handleClick(6)}/>
-                <Square  onClick={()=>handleClick(7)}/>
-                <Square  onClick={()=>handleClick(9)}/>
+                {
+                    squares.map((x, i) => [6, 7, 8].includes(i) ? <Square
+                        key={i}
+                        onClick={() => handleClick(i)}
+                        val={squares[i]}
+                        disabled={squares[i] != ""}
+                    /> : null
+                    )
+                }
             </div>
 
         </div>
