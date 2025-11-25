6e2cf4aface24a36926e4e972ad5309cfc4cba66
Antonio <antoniojsp@gmail.com>
2025-11-24 20:36:43 -0800

    good day, good weather

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
         {/* <TestWindowResize/> */}
         {/* <ScrollTopBottom /> */}
         {/* <ScrollToSection /> */}
-        <WeatherWidget />
+        {/* <WeatherWidget /> */}
     </div>
   )
 }
